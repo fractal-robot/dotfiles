3aeb7191709830172addde4117062a516a0914f0
fractal-robot <utsdxbur@proton.me>
2024-01-06 16:31:12 +0100

    adding new polybar modules and fixing mono font

---
+++ polybar/scripts/system-bluetooth-bluetoothctl.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+bluetooth_print() {
+    bluetoothctl | grep --line-buffered 'device\|#' | while read -r reply; do
+        if bluetoothctl show | grep -q "Powered: yes"; then
+            devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+
+            counter=0
+            for device in $devices_paired; do
+                device_info=$(bluetoothctl info "$device")
+
+                if echo "$device_info" | grep -q "Connected: yes"; then
+                    device_output=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+                    device_battery_percent=$(echo "$device_info" | grep "Battery Percentage" | awk -F'[()]' '{print $2}')
+
+                    device_output="$device_output $device_battery_percent%"
+                    fi
+
+                    if [ $counter -gt 0 ]; then
+                        printf ", %s" "$device_output"
+                    else
+                        printf " %s" "$device_output"
+                    fi
+
+                    counter=$((counter + 1))
+            done
+
+            printf '\n'
+        fi
+    done
+}
+
+bluetooth_toggle() {
+    if bluetoothctl show | grep -q "Powered: no"; then
+        bluetoothctl power on >> /dev/null
+        sleep 1
+
+        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+        echo "$devices_paired" | while read -r line; do
+            bluetoothctl connect "$line" >> /dev/null
+        done
+    else
+        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+        echo "$devices_paired" | while read -r line; do
+            bluetoothctl disconnect "$line" >> /dev/null
+        done
+
+        bluetoothctl power off >> /dev/null
+    fi
+}
+
+case "$1" in
+    --toggle)
+        bluetooth_toggle
+        ;;
+    *)
+        bluetooth_print
+        ;;
+esac
