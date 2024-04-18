572c8efcc2f070b93e319fd65c2d1ffcd8c74236
fractal-robot <utsdxbur@proton.me>
2024-04-18 23:42:25 +0200

    remaking my nvim config from scratch

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,32 @@
 if status is-interactive
-	starship init fish | source
 	set -g fish_greeting
 
-	alias wget="wget --hsts-file=\"$XDG_DATA_HOME/wget-hsts\""
+	fish_add_path /home/usr/.system/scripts/ -pP
+
+	starship init fish | source
+	zoxide init fish | source
+
+	fish_config theme choose rose-pine
+
+	# test $TERM != "tmux-256color"; and exec tmux
+
+	if type -q wget
+		alias wget="wget --hsts-file=\"$XDG_DATA_HOME/wget-hsts\""
+	end
 
 	abbr --position command --add s sudo 
 	abbr --position command --add c clear
-	abbr --position command --add v nvim
 	abbr --position command --add l exa
 	abbr --position command --add ll exa -1la
+	abbr --position command --add v nvim
+
+	# alias v open_nvim
+
+	fish_vi_key_bindings
+	set fish_cursor_default block # normal & visual mode
+	set fish_cursor_insert block
+	# set fish_cursor_replace_one underscore
+	# set fish_cursor_replace underscore
 end
 
 function fish_greeting
@@ -16,7 +34,12 @@ function fish_greeting
 	nerdfetch
 end
 
-fish_config theme choose rose-pine
+function open_nvim
+	kitty @ set-spacing padding=0
+	nvim $argv
+	kitty @ set-spacing padding=default
+end
 
-fish_add_path /home/usr/.system/scripts/ -P
-zoxide init fish | source
+
+
+
---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,23 @@
+return { 
+	'stevearc/oil.nvim',
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		require("oil").setup({
+			columns = {
+				"icon",
+				-- "mtime",
+				-- "permissions",
+				-- "size",
+			},
+
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true, 
+			-- :help prompt_save_on_select_new_entry
+		})
+
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end
+}
